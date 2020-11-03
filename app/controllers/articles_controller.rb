0cc488e98f1385eca5a8fc49e635aff61c2ca656
Antonio Braz <antoniovbraz@gmail.com>
2020-11-03 19:43:55 -0300

    creating new article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,19 @@ class ArticlesController < ApplicationController
     def index
       @articles = Article.all
     end
+
+    def def new
+      @article = Article.new
+    end
+
+    def def create
+      @article = Article.new(params[:article])
+      if @article.save
+        flash[:success] = "Object successfully created"
+        redirect_to @article
+      else
+        flash[:error] = "Something went wrong"
+        render 'new'
+      end
+    end
 end
