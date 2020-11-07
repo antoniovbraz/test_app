859e9509f0aa148d01e11a97baf8baa6febfac95
Antonio Braz <antoniovbraz@gmail.com>
2020-11-06 23:19:58 -0300

    Created partials and fixed some DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,50 +1,58 @@
 class ArticlesController < ApplicationController
-    def show
-      @article = Article.find(params[:id])
-    end
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
+  def show
+  end
 
-    def index
-      @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def new
-      @article = Article.new
-    end
+  def new
+    @article = Article.new
+  end
 
-    def edit
-      @article = Article.find(params[:id])
-    end
+  def edit
+  end
 
-    def create
-      @article = Article.new(params.require(:article).permit(:title, :description))
-      if @article.save
-        flash[:success] = "Article successfully created."
-        redirect_to @article
-      else
-        flash[:error] = "Something went wrong"
-        render 'new'
-      end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article successfully created."
+      redirect_to @article
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
     end
+  end
 
-    def update
-      @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
-        flash[:sucess] = "Article successfully updated"
-        redirect_to @article
-      else
-        flash[:error] = "Something went wrong"
-        render 'edit'
-      end
+  def update
+    if @article.update(article_params)
+      flash[:sucess] = "Article successfully updated"
+      redirect_to @article
+    else
+      flash[:error] = "Something went wrong"
+      render 'edit'
     end
+  end
 
-    def destroy
-      @article = Article.find(params[:id])
-      if @article.destroy
-        flash[:success] = 'Article was successfully deleted.'
-        redirect_to articles_url
-      else
-        flash[:error] = 'Something went wrong'
-        redirect_to articles_url
-      end
+  def destroy
+    if @article.destroy
+      flash[:success] = 'Article was successfully deleted.'
+      redirect_to articles_url
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to articles_url
     end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
