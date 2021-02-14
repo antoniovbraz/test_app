ef5a5eda3fcfff0aa82408a0651f1c66832ed4b7
Antonio Braz <antoniovbraz@gmail.com>
2021-02-14 00:43:15 +0000

    Create integration test for invalid category creation attempt

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
-  test "get new category form and create category" do
+   test "get new category form and create category" do
     get "/categories/new"
     assert_response :success
     assert_difference 'Category.count', 1 do
@@ -12,8 +12,8 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
-  
-   test "get new category form and reject invalid category submission" do
+
+  test "get new category form and reject invalid category submission" do
     get "/categories/new"
     assert_response :success
     assert_no_difference 'Category.count' do
